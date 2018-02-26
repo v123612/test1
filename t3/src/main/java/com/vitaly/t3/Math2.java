503759b52ce5e1aa382af51e43c8ab852f160a5d
vitaly <vitaly@vitaly>
2018-02-27 01:32:33 +0300

    First commit

---
+++ t3/src/main/java/com/vitaly/t3/Math2.java
@@ -0,0 +1,18 @@
+package com.vitaly.t3;
+
+public class Math2 {
+	public static int sumStatic(int a, int b) {
+		return a+b;
+	}
+	
+	public int sum(int a, int b) {
+		return sumStatic(a,b);
+	}
+	
+	public static int mult(int a, int b) {
+		return a*b;
+	}  
+	
+	
+
+}
---
+++ t3/src/test/java/com/vitaly/t3/Math2Test.java
@@ -0,0 +1,15 @@
+package com.vitaly.t3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Math2Test {
+
+	@Test
+	public void testSumStatic() {
+		//fail("Not yet implemented");
+		assertTrue(true);
+	}
+
+}
